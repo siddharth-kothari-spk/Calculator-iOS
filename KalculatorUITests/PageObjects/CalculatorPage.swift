8ffe3a1bedfa3ecdae2ebe9d3e399989d8348816
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-04-30 14:09:23 +0530

    created test classes

---
+++ KalculatorUITests/PageObjects/CalculatorPage.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorPage.swift
+//  KalculatorUITests
+//
+//  Created by Siddharth Kothari on 30/04/24.
+//  Copyright © 2024 Jogendra Singh. All rights reserved.
+//
+
+import Foundation
